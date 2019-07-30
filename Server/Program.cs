6917ede835a946e6197e836e0858790bf649c9dd
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2019-07-30 12:41:04 +0300

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket s = new Socket(
+				AddressFamily.InterNetwork,
+				SocketType.Stream,
+				ProtocolType.Tcp
+			);
+			IPAddress ip = IPAddress.Parse("127.0.0.1");
+			IPEndPoint ep = new IPEndPoint(ip, 3000);
+
+			try
+			{
+				s.Bind(ep);
+				s.Listen(10);
+
+				while (true)
+				{
+					Socket ns = s.Accept();
+					Console.WriteLine("Recive from: " + ns.RemoteEndPoint.ToString());
+					byte[] buffer = new byte[1024];
+					string result = "";
+
+					while (true)
+					{
+						result += Encoding.ASCII.GetString(buffer, 0, ns.Receive(buffer));
+						Console.WriteLine($"Recive: {result}");
+						if (result.IndexOf("<EOF>") > -1)
+						{
+							break;
+						}
+					}
+
+					result = result.Replace("<EOF>", "");
+					Console.WriteLine("Send to: " + ns.RemoteEndPoint.ToString());
+
+					result += " frog!" + "<EOF>";
+					Console.WriteLine("Send: " + result + "\n");
+
+					byte[] msg = Encoding.ASCII.GetBytes(result);
+					ns.Send(msg);
+					ns.Shutdown(SocketShutdown.Both);
+					ns.Close();
+				}
+			}
+			catch (SocketException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
---
+++ dotnet-NW_HT/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnet_NW_HT
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPAddress ip = IPAddress.Parse("127.0.0.1");
+			IPEndPoint ep = new IPEndPoint(ip, 3000);
+			Socket s = new Socket(
+				AddressFamily.InterNetwork,
+				SocketType.Stream,
+				ProtocolType.Tcp
+			);
+			try
+			{
+				s.Connect(ep);
+				if (s.Connected)
+				{
+					String strSend;
+
+					for (int i = 0; i < 5; i++)
+					{
+						strSend = (i+1).ToString() + ". Hello!<EOF>";
+						s.Send(System.Text.Encoding.ASCII.GetBytes(strSend));
+						Console.WriteLine("Send: " + strSend);
+					}
+
+					byte[] buffer = new byte[1024];
+					string result = "";
+
+					do
+					{
+						try
+						{
+							result += System.Text.Encoding.ASCII.GetString(buffer, 0, s.Receive(buffer));
+						}
+						catch (Exception)
+						{
+							break;
+						}
+
+					} while (s.Receive(buffer) > 0);
+					Console.WriteLine(result);
+				}
+			}
+			catch (SocketException ex)
+			{
+				Console.WriteLine(ex.Message);
+				foreach (var item in ex.StackTrace)
+				{
+					Console.WriteLine();
+				}
+				throw;
+			}
+
+			Console.Read();
+		}
+	}
+}
